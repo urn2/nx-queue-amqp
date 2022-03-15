9ddf089e539effc8699608d6f5ac36c1c8384b49
Vea <vea.fu@vertuonline.com>
2022-03-15 16:38:07 +0800

    0.0.3

--- src/helpers/amqp.php
+++ src/helpers/amqp.php
@@ -1,6 +1,8 @@
 <?php
 namespace nx\helpers\queue;
 
+use PhpAmqpLib\Channel\AMQPChannel;
+
 class amqp{
 	protected $config=[];
 	protected $conn=null;
@@ -8,14 +10,14 @@ class amqp{
 	public function __construct($config){
 		$this->config=$config;
 	}
-	protected function channel(){
+	protected function channel():AMQPChannel{
 		if(null === $this->conn){
 			$this->conn=new \PhpAmqpLib\Connection\AMQPStreamConnection(...$this->config['conn']);
 			$this->channel =$this->conn->channel();
 		}
 		return $this->channel;
 	}
-	protected function declareRoute($key):bool{
+	protected function declareRoute($key):void{
 		$config=$this->config['route'][$key] ?? null;
 		if(null === $config) throw new \Error('未定义的route配置 '.$key);
 		if($config['declare']){
@@ -25,13 +27,13 @@ class amqp{
 			$this->channel()
 				->queue_declare($config['queue']['name'], $config['queue']['passive'] ?? false, $config['queue']['durable'] ?? false, $config['queue']['exclusive'] ?? true,
 					$config['queue']['auto_delete'] ?? false);
-			return $this->channel()->queue_bind($config['queue']['name'], $config['exchange']['name'], $key);
+			$this->channel()->queue_bind($config['queue']['name'], $config['exchange']['name'], $key);
 		}
-		return true;
 	}
 	public function publish($key, $data):bool{
-		$this->declareRoute($key);
 		try{
+			$this->declareRoute($key);
+
 			$_data =is_string($data) ?$data :json_encode($data, JSON_UNESCAPED_UNICODE);
 			$msg=new \PhpAmqpLib\Message\AMQPMessage($_data, ["delivery_mode"=>\PhpAmqpLib\Message\AMQPMessage::DELIVERY_MODE_NON_PERSISTENT]);
 			$this->channel()->basic_publish($msg, $this->config['route'][$key]['exchange']['name'], $key);
